32b02fc50b5f1914ef9a342cd043363ea09200a0
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-07-02 12:00:49 +0530

    feat: add execution process of javaScript

---
+++ 01_execution.js
@@ -0,0 +1,26 @@
+// execution in js is done in two phases 
+// 1. memory creation phase
+// 2. code execution phase
+
+// before execute the code js scan it to create memory 
+// for example:-
+
+// code:- 
+let a = 10;
+function product(x){
+    return x*x
+}
+let b = 20
+console.log(product(a+b))
+
+// memory creation phase:- 
+
+// a = undefined
+// add = function(){...}
+// b = undefined
+
+// code execution phase:-
+// a = 10   // in memory a = 10
+// ad function // nothing changed because already stored entire function in memory
+// b = 20    // b = 20
+
